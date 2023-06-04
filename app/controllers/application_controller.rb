ecc44ce1367ed120aa5995b6b7055db0ebea4de8
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-06-04 05:35:31 +0000

    [add]Log inページの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 def after_sign_in_path_for(resource)
     books_path
-  end
+end
 
-  def after_sign_out_path_for(resource)
+def after_sign_out_path_for(resource)
     root_path
-  end
+end
 
 
 
