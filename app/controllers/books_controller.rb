c055f1606b93a91db57278e81fa7d2dfae690c43
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-06-08 14:00:05 +0000

    [add]Bookのdestroy作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,12 +28,18 @@ class BooksController < ApplicationController
     redirect_to book_path(@book.id)
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
 
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :body,)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :books, only:[:create, :index, :show, :edit, :update]
+  resources :books, only:[:create, :index, :show, :edit, :update, :destroy]
   resources :users, only:[:index, :show, :edit, :update]
 
   get 'home/about' => 'homes#about', as: 'about'
