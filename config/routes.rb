60f3e031ccc54aac21c224e2f53e31ae337fc5a6
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-11-12 11:13:49 +0000

    検索機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   end
 
   get 'home/about' => 'homes#about', as: 'about'
+  get 'search' => 'searches#search'
+  get 'search_result' => 'searches#search_result'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
